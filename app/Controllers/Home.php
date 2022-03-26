03fd893a47c2e83f10d7727ed81de0f6799228d8
DenkCool <93751741+MrArbai@users.noreply.github.com>
2022-03-26 15:51:57 +0700

    qe

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,10 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        $komik = $this->ProdukModel->findall();
+        // $produk = $this->ProdukModel->findall();
         $data = [
             'title' => 'Home | Mikonots',
-            'komik' => $this->ProdukModel->getproduk()
+            'produk' => $this->ProdukModel->getproduk()
         ];
         return view('Konten/index', $data);
     }
